7ecae2ca389da07c8b69650a83478b634ca9cb17
Josh Grime <33323152+joshgrime@users.noreply.github.com>
2019-12-17 00:30:29 +0000

    Update my-profile-data.js

--- private-api/my-profile-data.js
+++ private-api/my-profile-data.js
@@ -10,7 +10,7 @@ module.exports = {
         "Key": {
             "id":id
         },
-        "ProjectionExpression": "id, host, games, #lvl, twitchAuthed, twitchName, twitchAvatar, coach, casual, vs, console, exp, email, username, displayName, coins, locale, avatar, bio, herocode",
+        "ProjectionExpression": "id, host, games, #lvl, twitchAuthed, twitchName, twitchAvatar, coach, casual, vs, console, exp, email, username, displayName, coins, locale, avatar, bio, herocode, stripeConnected",
         "ExpressionAttributeNames": {
           "#lvl":"level"
         }
